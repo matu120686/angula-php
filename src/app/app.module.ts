89c7b6a56b9444f431d11d9476668bcb5d51478f
Matias Olivera <matu120686@gmail.com>
2022-12-12 20:06:55 -0300

    creo primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarEmpleadoComponent,
+    EditarEmpleadoComponent,
+    ListarEmpleadoComponent
   ],
   imports: [
     BrowserModule
