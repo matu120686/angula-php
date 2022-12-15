8aa25196bf7f33b3358f2f91c1d519d583c63429
Matias Olivera <matu120686@gmail.com>
2022-12-15 18:41:46 -0300

    Create Views Employee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+// Module dependencies Component
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
+
+
 const routes: Routes = [
-  
+  {path: '', pathMatch:'full', redirectTo:'agregar-empleado'},
+  {path: 'agregar-empleado', component:AgregarEmpleadoComponent},
+  {path: 'listar-empleado', component:ListarEmpleadoComponent},
+  {path: 'editar-empleado/:id', component:EditarEmpleadoComponent},
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
